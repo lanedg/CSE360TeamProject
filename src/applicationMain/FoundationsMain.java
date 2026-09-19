ae60cb416f42c0ab2bfc96228ef1214237a34c05
Lane Gebbie <166470962+lanedg@users.noreply.github.com>
2026-09-18 19:39:40 -0700

    Test commit

--- src/applicationMain/FoundationsMain.java
+++ src/applicationMain/FoundationsMain.java
@@ -9,7 +9,7 @@ import javafx.scene.control.Alert.AlertType;
 
 /*******
  * <p> Title: FoundationsMain Class </p>
- *
+ * test
  * <p> Description: This is the Main Class that launches the Foundations demonstration
  * application.  This is a JavaFX application designed to serve as a foundation for the CSE360
  * Individual Homework and Team Project assignments and demonstrate the use of the following:</p>
