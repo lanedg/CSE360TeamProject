464a85e6c0a42179f3303803b1d977acb6a12f79
ckous <ckous@Kosik_PC>
2026-09-18 19:23:18 -0700

    just a test

--- src/applicationMain/FoundationsMain.java
+++ src/applicationMain/FoundationsMain.java
@@ -56,6 +56,9 @@ import javafx.scene.control.Alert.AlertType;
  * @version 3.00	2025-08-17 Rewrite of this application for the Fall offering of CSE 360 and
  * other ASU courses.
  * @version 3.02	2025-12-17 Enhancements in support of Spring 2026
+ * 
+ * 
+ * THIS IS A GIT TEST
  */
 
 public class FoundationsMain extends Application {
